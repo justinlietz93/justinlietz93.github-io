d6e0c275d245589ffa674e64dfa17b78d62639af
Justin Lietz <jlietz93@gmail.com>
2024-02-07 22:37:24 -0600

    Added dynamic welcome to index.html

--- scripts.js
+++ scripts.js
@@ -1,1 +1,16 @@
 console.log("Javascript successfully linked.")
+
+const hours = new Date().getHours() // get the current hour
+
+const isMorning = hours >= 4 && hours < 12 // is it morning?
+const isAfternoon = hours >= 12 && hours < 17 // is it afternoon?
+const isEvening = hours >= 17 || hours < 4 // is it evening?
+
+// Checks for current time and then displays appropriate greeting
+if (isMorning) {
+  document.getElementById("welcome").innerHTML = "Good morning!"
+} else if (isAfternoon) {
+  document.getElementById("welcome").innerHTML = "Good afternoon!"
+} else if (isEvening) {
+  document.getElementById("welcome").innerHTML = "Good evening!"
+}
